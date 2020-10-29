61e439ce60f301f89248f6c0e92a3bd0a026d72f
Ethan Cowan <ethan.cowan@gmail.com>
2020-10-28 22:39:47 -0600

    Alter transparent generosity

--- src/pages/transparent-generosity.js
+++ src/pages/transparent-generosity.js
@@ -8,9 +8,9 @@ export default function TransparentGenerosity() {
       <h1>Transparent Generosity</h1>
       <p>Because of my own history of learning through Feldenkrais' movement lessons, I feel deep satisfaction when sharing the sequences that have been so engaging for me. They are beautiful human compositions that should be more widely known, enjoyed and elaborated.</p>
       <p>At the same time, I am a member of a society where capitalism runs amok, and in some sense, cash rules everything around me.</p>
-      <p>I don't want everything I do to be reduced to monetized interactions. That's part of why I offer what lessons I can in a spirit of transparent generosity. I ask that the people who study with me consider how they might do their own trials and errors with transparent generosity [doesn't have to be perfect]. Many people offer money to start. Eventually, some adventurous few take the time to think about, What else would I enjoy sharing? Some experiment with spreading generosity to different places and people in our web of connections.</p>
-      <p>Everyone has gifts to offer. In the spirit of Feldenkrais, I am saying that half of Health is the ability to live out unavowed dreams. What if part of living our unavowed dreams is uncovering our gifts and our capacities to share them? I think so.</p>
-      <h4>This model was inspired by Vince Horn's <a href="https://meta.buddhistgeeks.org/about/transgenerosity">transparent generosity</a>.</h4>
+      <p>I don't want everything I do to be reduced to monetized interactions, so I offer what lessons in a spirit of transparent generosity. I ask that the people who study with me consider how they might engage with the idea of transparent generosity [and I remind them, it doesn't have to be perfect]. Many people offer money to start. Eventually, some adventurous few take the time to think about what else they'd enjoy sharing? Some experiment with "paying it forward," or sideways.</p>
+      <p>Everyone has gifts to offer. In the spirit of Feldenkrais, I am saying that half of Health is the ability to live our unavowed dreams. I'm experimenting with the idea that maybe some of our unavowed dreams get lived out in the sharing of gifts.</p>
+      <h4>This model is inspired by Vince Horn's <a href="https://meta.buddhistgeeks.org/about/transgenerosity">transparent generosity</a>.</h4>
     </Layout>
   )
 }
