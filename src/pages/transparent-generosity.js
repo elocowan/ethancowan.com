3d1adca17cdc3f01ef3fc51723b8f38e1440039b
Ethan Cowan <ethan.cowan@gmail.com>
2020-10-28 22:41:19 -0600

    Update transparent generosity

--- src/pages/transparent-generosity.js
+++ src/pages/transparent-generosity.js
@@ -6,7 +6,7 @@ export default function TransparentGenerosity() {
   return (
     <Layout>
       <h1>Transparent Generosity</h1>
-      <p>Because of my own history of learning through Feldenkrais' movement lessons, I feel deep satisfaction when sharing the sequences that have been so engaging for me. They are beautiful human compositions that should be more widely known, enjoyed and elaborated.</p>
+      <p>Because of my own history of learning through Feldenkrais' movement lessons, I feel deep satisfaction when sharing what has been so engaging for me. Feldenkrais lessons are beautiful human compositions that should be more widely known, enjoyed and elaborated.</p>
       <p>At the same time, I am a member of a society where capitalism runs amok, and in some sense, cash rules everything around me.</p>
       <p>I don't want everything I do to be reduced to monetized interactions, so I offer what lessons in a spirit of transparent generosity. I ask that the people who study with me consider how they might engage with the idea of transparent generosity [and I remind them, it doesn't have to be perfect]. Many people offer money to start. Eventually, some adventurous few take the time to think about what else they'd enjoy sharing? Some experiment with "paying it forward," or sideways.</p>
       <p>Everyone has gifts to offer. In the spirit of Feldenkrais, I am saying that half of Health is the ability to live our unavowed dreams. I'm experimenting with the idea that maybe some of our unavowed dreams get lived out in the sharing of gifts.</p>
