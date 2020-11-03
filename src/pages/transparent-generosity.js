169e7b7d817c36f8f7027c6f17531fcdacbdeb0f
Ethan Cowan <ethan.cowan@gmail.com>
2020-11-02 18:35:41 -0700

    Change wording in transparent generosity

--- src/pages/transparent-generosity.js
+++ src/pages/transparent-generosity.js
@@ -9,7 +9,7 @@ export default function TransparentGenerosity() {
       <p>I often feel deep satisfaction sharing Feldenkrais lessons. They are beautiful, humane compositions that should be more widely experienced, enjoyed and elaborated.</p>
       <p>At the same time, I am a member of a society where capitalism runs amok. As Method Man said back in 1993, "cash rules everything around me."</p>
       <p>I don't want everything I do reduced to money, and yet I recognize that money plays an important part in our reality. To look for a balance, I offer lessons in a spirit of transparent generosity. I ask that the people who study with me consider how they might engage with the idea of transparent generosity [and I remind them, it doesn't have to be perfect]. Many people offer money to start. Some take the time to think about what else they'd enjoy sharing. Some experiment with "paying it forward," or sideways.</p>
-      <p>Everyone has gifts to offer. In the spirit of Feldenkrais, I am saying that half of Health is the ability to live our unavowed dreams. Transparent Generosity is an experiment to test whether some of our unavowed dreams may be lived out in the sharing of gifts.</p>
+      <p>Everyone has gifts to offer. Feldenkrais said half of Health is the ability to live our unavowed dreams. Transparent Generosity is an experiment to test whether some of our unavowed dreams may be lived out in the sharing of gifts.</p>
       <p>If you'd like to talk about working together, {" "}
         <a href="https://app.acuityscheduling.com/schedule.php?owner=16756071">
           schedule an intro call
